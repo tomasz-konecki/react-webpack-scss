bc474e47d86577c946beb8149fa42f5ade370851
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2018-04-06 10:13:16 +0200

    Initial commit

--- src/components/BootTest.js
+++ src/components/BootTest.js
@@ -2,80 +2,58 @@ import React from "react";
 
 const BootTest = () => {
 	return (
-		<div id="accordion">
-			<div className="card">
-				<div
-					className="card-header"
-					id="headingOne"
-					data-toggle="collapse"
-					data-target="#collapseOne"
-					aria-expanded="true"
-					aria-controls="collapseOne"
-				>
-					<h5>Collapsible Group Item #1</h5>
-				</div>
+		<div>
+			<h1>
+				Example heading <span className="badge badge-secondary">New</span>
+			</h1>
 
-				<div id="collapseOne" className="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-					<div className="card-body">
-						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon
-						officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3
-						wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et.
-						Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan
-						excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt
-						you probably haven't heard of them accusamus labore sustainable VHS.
-					</div>
+			<div className="container box-1">
+				<h2>Fixed container</h2>
+			</div>
+			<div className="fluid-container box-2">
+				<h2>Fixed container</h2>
+			</div>
+			<div className="container">
+				<div className="row">
+					<div className="col-sm">Column 1</div>
+					<div className="col-sm">Column 1</div>
+					<div className="col-sm">Column 1</div>
+					<div className="col-sm">Column 1</div>
 				</div>
 			</div>
-			<div className="card">
-				<div className="card-header" id="headingTwo">
-					<h5 className="mb-0">
-						<button
-							className="btn btn-link collapsed"
-							data-toggle="collapse"
-							data-target="#collapseTwo"
-							aria-expanded="false"
-							aria-controls="collapseTwo"
-						>
-							Collapsible Group Item #2
-						</button>
-					</h5>
-				</div>
-				<div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-					<div className="card-body">
-						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon
-						officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3
-						wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et.
-						Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan
-						excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt
-						you probably haven't heard of them accusamus labore sustainable VHS.
-					</div>
-				</div>
-			</div>
-			<div className="card">
-				<div className="card-header" id="headingThree">
-					<h5 className="mb-0">
-						<button
-							className="btn btn-link collapsed"
-							data-toggle="collapse"
-							data-target="#collapseThree"
-							aria-expanded="false"
-							aria-controls="collapseThree"
-						>
-							Collapsible Group Item #3
-						</button>
-					</h5>
-				</div>
-				<div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-					<div className="card-body">
-						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon
-						officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3
-						wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et.
-						Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan
-						excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt
-						you probably haven't heard of them accusamus labore sustainable VHS.
-					</div>
-				</div>
-			</div>
+			<code>&lt;section&gt;</code>
+			<code>&lt;section&gt;</code>
+
+			<table className="table">
+				<thead>
+					<tr>
+						<th scope="col">#</th>
+						<th scope="col">First</th>
+						<th scope="col">Last</th>
+						<th scope="col">Handle</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th scope="row">1</th>
+						<td>Mark</td>
+						<td>Otto</td>
+						<td>@mdo</td>
+					</tr>
+					<tr>
+						<th scope="row">2</th>
+						<td>Jacob</td>
+						<td>Thornton</td>
+						<td>@fat</td>
+					</tr>
+					<tr>
+						<th scope="row">3</th>
+						<td>Larry</td>
+						<td>the Bird</td>
+						<td>@twitter</td>
+					</tr>
+				</tbody>
+			</table>
 		</div>
 	);
 };

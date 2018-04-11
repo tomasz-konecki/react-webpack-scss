5887be209af35e2fa6e11f2b5a22f46d0773e963
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2018-04-11 09:08:29 +0200

    Updated code

--- src/components/AnimatedHeight.js
+++ src/components/AnimatedHeight.js
@@ -21,35 +21,59 @@ class AnimatedHeight extends Component {
     let direction = this.state.iconDirection;
     return (
       <div className="container">
-        <div className="icon" onClick={this.handleClick}>
-          <i className={`fas fa-angle-${direction}`} />
-        </div>
-        <div className={["rectangle", this.state.rectangleClass].join(" ")}>
-          <p>
-            Proident irure cillum consequat magna nulla irure aliqua minim sit.
-            Ex aute magna in minim excepteur labore aliqua amet aliquip minim do
-            fugiat occaecat ad. Lorem proident amet ea eu voluptate do laborum
-            laborum culpa occaecat. Consequat eu laborum aliqua esse duis elit
-            anim excepteur laborum nulla voluptate. Qui minim veniam labore enim
-            minim ex consectetur ex fugiat excepteur adipisicing. Ipsum velit
-            sint sunt in.
-          </p>
-        </div>
-        <br />
-        <div className="icon" onClick={this.handleClick}>
-          <i className={`fas fa-angle-${direction}`} />
-        </div>
-        <div className={["rectangle", this.state.rectangleClass].join(" ")}>
-          <p>
-            Proident irure cillum consequat magna nulla irure aliqua minim sit.
-            Ex aute magna in minim excepteur labore aliqua amet aliquip minim do
-            fugiat occaecat ad. Lorem proident amet ea eu voluptate do laborum
-            laborum culpa occaecat. Consequat eu laborum aliqua esse duis elit
-            anim excepteur laborum nulla voluptate. Qui minim veniam labore enim
-            minim ex consectetur ex fugiat excepteur adipisicing. Ipsum velit
-            sint sunt in.
-          </p>
-        </div>
+        <table>
+          <thead>Table</thead>
+          <tbody>
+            <tr>
+              <td className="icon">
+                <i
+                  className={`fas fa-angle-${direction}`}
+                  onClick={this.handleClick}
+                />
+              </td>
+              <td>
+                <div
+                  className={["rectangle", this.state.rectangleClass].join(" ")}
+                >
+                  <p>
+                    Proident irure cillum consequat magna nulla irure aliqua
+                    minim sit. Ex aute magna in minim excepteur labore aliqua
+                    amet aliquip minim do fugiat occaecat ad. Lorem proident
+                    amet ea eu voluptate do laborum laborum culpa occaecat.
+                    Consequat eu laborum aliqua esse duis elit anim excepteur
+                    laborum nulla voluptate. Qui minim veniam labore enim minim
+                    ex consectetur ex fugiat excepteur adipisicing. Ipsum velit
+                    sint sunt in.
+                  </p>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td className="icon">
+                <i
+                  className={`fas fa-angle-${direction}`}
+                  onClick={this.handleClick}
+                />
+              </td>
+              <td>
+                <div
+                  className={["rectangle", this.state.rectangleClass].join(" ")}
+                >
+                  <p>
+                    Proident irure cillum consequat magna nulla irure aliqua
+                    minim sit. Ex aute magna in minim excepteur labore aliqua
+                    amet aliquip minim do fugiat occaecat ad. Lorem proident
+                    amet ea eu voluptate do laborum laborum culpa occaecat.
+                    Consequat eu laborum aliqua esse duis elit anim excepteur
+                    laborum nulla voluptate. Qui minim veniam labore enim minim
+                    ex consectetur ex fugiat excepteur adipisicing. Ipsum velit
+                    sint sunt in.
+                  </p>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     );
   }

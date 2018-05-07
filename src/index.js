9b894d332463b7660b536f5686bad82f817857b0
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2018-05-07 15:38:22 +0200

    Added functions

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
 
 import App from "./App";
+// render(<App />, document.getElementById("app"));
 
 let person = {
   firstname: "John",
@@ -14,6 +15,37 @@ let person = {
   }
 };
 
-console.log(person.getFullName());
+let logName = function(lang1, lang2) {
+  console.log("Logged: " + this.getFullName());
+  console.log("Arguments: " + lang1 + " " + lang2);
+  console.log("--------------------");
+};
 
-// render(<App />, document.getElementById("app"));
+let logPersonName = logName.bind(person);
+
+logPersonName("en");
+
+logName.call(person, "en", "es");
+logName.apply(person, ["hu", "cz"]);
+
+(function(lang1, lang2) {
+  console.log("Logged: " + this.getFullName());
+  console.log("Arguments: " + lang1 + " " + lang2);
+  console.log("--------------------");
+}.apply(person, ["pl", "fr"]));
+
+//function borrowing
+let person2 = {
+  firstname: "Paul",
+  lastname: "King"
+};
+
+console.log(person.getFullName.call(person2));
+
+//function currying
+function multiply(a, b) {
+  return a * b;
+}
+
+let multiplyByTwo = multiply.bind(this, 3);
+console.log(multiplyByTwo(5));
